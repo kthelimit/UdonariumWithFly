a35298693409f46f043ca146cb912153aa45498a
K <73793167+kthelimit@users.noreply.github.com>
2022-10-30 01:34:27 +0900

    Update stand-setting.component.ts

--- src/app/component/stand-setting/stand-setting.component.ts
+++ src/app/component/stand-setting/stand-setting.component.ts
@@ -111,7 +111,7 @@ export class StandSettingComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   updatePanelTitle() {
-    this.panelService.title = this.character.name + ' のスタンド設定';
+    this.panelService.title = this.character.name + ' 의 스탠딩 설정';
   }
 
   add() {
@@ -126,8 +126,8 @@ export class StandSettingComponent implements OnInit, OnDestroy, AfterViewInit {
     });
     if (!this.character || !this.character.standList) return;
     this.modalService.open(ConfirmationComponent, {
-      title: 'スタンド設定の削除', 
-      text: 'スタンド設定を削除しますか？',
+      title: '스탠딩 설정의 삭제', 
+      text: '스탠딩 설정을 삭제합니까?',
       type: ConfirmationType.OK_CANCEL,
       materialIcon: 'person_off',
       action: () => {
@@ -185,29 +185,29 @@ export class StandSettingComponent implements OnInit, OnDestroy, AfterViewInit {
     let coordinate = this.pointerDeviceService.pointers[0];
     let option: PanelOption = { left: coordinate.x, top: coordinate.y, width: 600, height: 590 };
     let textView = this.panelService.open(TextViewComponent, option);
-    textView.title = 'スタンド設定ヘルプ';
+    textView.title = '스탠딩 설정 도움말';
     textView.text = 
-`　キャラクターのスタンドの名前、位置と画像の高さ（それぞれ画面サイズに対する相対指定）、チャット送信時にスタンドが表示される条件を設定できます。
+`　캐릭터의 스탠딩 이름, 위치와 이미지 높이(각각 화면 크기에 대한 상대 지정), 채팅 발송 시 스탠딩이 표시되는 조건을 설정할 수 있습니다.
 
-　スタンドに名前を設定した場合、チャットウィンドウ、チャットパネルのリストに表示され、選択可能になります。また、タグを設定した場合、異なるタグでは同じキャラクターであっても登場、退去のアニメーションが行われます。
+스탠딩에 이름을 설정한 경우, 채팅 윈도우, 채팅 패널의 리스트에 표시되며 선택할 수 있게 됩니다. 또, 태그를 설정한 경우, 다른 태그에서는 같은 캐릭터라고 하더라도 등장, 퇴장의 애니메이션이 진행됩니다.
 
-　画像の位置と高さは個別指定も可能です、位置の個別指定はチェックなし、高さは0とした場合に全体の設定が使用されます。縦位置調整(AdjY)は、スタンド画像の高さに対する相対指定となります（例えば、-50%とすると画像の下半分が画面端より下に隠れます）。
+　화상의 위치와 높이는 개별 지정도 가능합니다. 위치의 개별 지정은 체크 없음, 높이는 0으로 한 경우에 전체의 설정이 사용됩니다. 세로 위치 조정(AdjY)은 스탠딩 화상의 높이에 대한 상대지정이 됩니다. (예를 들어 -50%로 하면 화상의 하반신이 화면단보다 아래에 가려집니다.)
 
-　条件の「指定画像」はチャット送信時のキャラクター画像あるいは顔ICです。また、特別な条件として常に、チャットテキストの末尾が"@退去"または"@farewell"の場合は、そのキャラクターのスタンドを退去させます。
+　조건의 「지정화상」은 채팅 송신 시의 캐릭터 화상 또는 얼굴 IC입니다. 또 특별한 조건으로서 항상 채팅 텍스트의 끝이 "@퇴장" 또는 "@farewell"인 경우, 그 캐릭터의 스탠딩이 사라집니다.
 
-　優先順位は高いものから
+　우선순위가 높은 것부터
 
-　　１. "@退去"、"@farewell"による退去
-　　２. チャットウィンドウ、チャットパネルのリストで選択した名前
-　　３. 「指定画像 かつ チャット末尾」
-　　４. 「指定画像 または チャット末尾」
-　　５. 「チャット末尾」
-　　６. 「指定画像」
+　　１. "@퇴장"、"@farewell"에 의한 퇴장
+　　２. 채팅 윈도우, 채팅 패널의 리스트에서 선택한 이름
+　　３. 「지정 이미지 동시에 채팅의 말미(끝)」
+　　４. 「지정 이미지 또는 채팅의 말미(끝)」
+　　５. 「채팅의 말미(끝)」
+　　６. 「지정화상」
 
-　どの条件も満たさない場合「デフォルト」のものが使用され、同じ優先順位の条件が複数ある場合はランダムで1つが選択されます。
+　어떤 조건도 만족하지 못한 경우 「디폴트」의 것이 사용되며 동일한 우선순위의 조건이 여러개인 경우 무작위로 하나가 선택됩니다.
 
-　チャット末尾一致を判定する際、全角半角、アルファべットの大文字小文字は区別されません。
-　また、"@退去"、"@farewell"による退去時、あるいは"@笑い"のように先頭が"@"で始まる条件を設定している場合、（スタンドの有効無効、条件を満たすかに関わらず）そのキャラクターでの送信時に、条件に一致するチャットテキスト末尾の@以下は切り落とされます。`;
+　채팅 끝 일치 판정 시, 전각반각, 알파벳 대소문자는 구분되지 않습니다.
+또  "@퇴장" 또는 "@farewell"에 의한 퇴장시 혹은 "@웃음"과 같이 선두가 "@"로 시작하는 조건을 설정한 경우, (스탠드의 유효무효, 조건을 만족시키는가에 관계없이) 그 캐릭터로 송신할 때 조건에 일치하는 채팅 끝의 @이하는 잘립니다.`;
   }
 
   private imageElementToFile(dataElm: DataElement): ImageFile {
